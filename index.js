01805bc91567814eeb2a65ac32818811f2109db5
18OJohnston.ACC <18ojohnston.acc@lwetb.ie>
2023-05-22 11:55:37 +0100

    bob

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 addEventListener("DOMContentLoaded", (e) => {
+	let cc_num = document.getElementById('cc_number');
+	let cvv = document.getElementById('cvv');
+	[document.getElementById('phone'), cc_num, cvv].forEach((x) => {
+		x.addEventListener('keypress', (e) => {
+			code = (e.keyCode ? e.keyCode : e.which);
+			if (code >= 48 && code <= 57 || code === 43)  {
+				return;
+			}
+			e.preventDefault();
+		});
+	});
+});
+
+
+function check_luhns() {
+	var numbers = cc_num.value.split('').map((i) => Number(i));
 	
-})
+	var check_num = numbers.splice(numbers.length-1, 1);
+	
+	numbers = numbers.reverse();
+	
+	
+	numbers.filter((_, i) => Boolean(i&1)).forEach((value) => {
+		var n = value*2;
+		if (n>9) {n-=9};
+		check_num += n;
+	}
+	
+	return check_num%10==0;
+}
+
+function verify_number() {
+	
+}
